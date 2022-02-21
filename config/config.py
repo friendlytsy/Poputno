f2c2fdb42114c274dbd95718828b01ff04a192d5
friendlytsy <as.tsygankov@icloud.com>
2022-02-21 12:50:19 +0300

    Изменения в конфиге

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-CRIMGOBOT_TOKEN = '5019874620:AAGNtS_qhsu59PLSeMSbYWnnoy4Aac8m1ZU'
+CRIMGOBOT_TOKEN = '5037196471:AAEEhkxZTDkyZYewebqIjaNvqo0JwmgyxK0'
 
 PAYMENTS_PROVIDER_TOKEN='381764678:TEST:32687'
 SHUTTLE_IMAGE_URL = 'https://ecotechnica.com.ua/images/-foto4/987-Jaguar-Land-Rover_vector-ecotechnicacomua-1.jpg'
@@ -7,6 +7,6 @@ DATABASE = {
     'host': 'localhost',
     'port': '5432',
     'username': 'postgres',
-    'password': 'postgres',
+    'password': 'et#$q9LNJfU*',
     'database': 'crimgo'
 }
