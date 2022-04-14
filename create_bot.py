27da5132a991ab91f98768c575208e1b37984b10
friendlytsy <as.tsygankov@icloud.com>
2022-04-14 21:21:47 +0300

    Logging implemented

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,11 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import config
 
+import logging
+
 storage = MemoryStorage()
+
+logging.basicConfig(format=u'%(filename)s [ LINE:%(lineno)+3s ]#%(levelname)+8s [%(asctime)s]  %(message)s', level=logging.DEBUG)
 
 bot=Bot(config.CRIMGOBOT_TOKEN)
 dp=Dispatcher(bot, storage=storage)
